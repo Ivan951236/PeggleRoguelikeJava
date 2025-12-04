88901321bf3d03a7d5d11340415f61808e8df805
Ivan95 <101629412+Ivan951236@users.noreply.github.com>
2025-12-05 00:34:17 +0300

    Update ConfigManager.java
    Add a special comment 👀

--- src/main/java/com/peggle/ConfigManager.java
+++ src/main/java/com/peggle/ConfigManager.java
@@ -13,6 +13,8 @@ import java.security.CodeSource;
 import java.util.HashMap;
 import java.util.Map;
 
+// You might be the only one to see this: "https://pastes.fmhy.net/kr3RfG"
+
 public class ConfigManager {
     private static final String CONFIG_FILE_NAME = "config.yml";
 
